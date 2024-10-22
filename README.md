e108e722332112d663b6ebbdc191566dc1ab9bae
Keese Phillips <95185935+keesephillips@users.noreply.github.com>
2024-10-21 22:05:21 -0400

    added model

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# applied_computer_vision_project_2
- 
+# Applied Computer Vision Project 2
+
+## Project 2 - Object Detection
+Keese Phillips
+  
+Attribution: 
+[Modern-Computer-Vision-with-PyTorch](https://github.com/PacktPublishing/Modern-Computer-Vision-with-PyTorch)
+
+> For information regarding the project methodology or evaluation please see the [Project 2 Report](./project_2.pdf)
--- make_dataset.py
+++ make_dataset.py
@@ -11,44 +11,37 @@ import random
 
 def scrape_images(query, num_images, save_path):
     print(f"\nDownloading {query}")
+    
     imgs_saved = 0
     page = 1
-    # Set up your headers here
+    
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
     }
 
-    # Add headers using CDP
     driver = webdriver.Edge(r"C:\\Program Files\\msedgedriver.exe")
     driver.request_interceptor = lambda request: request.headers.update(headers)
 
-    # This is the step for maximizing browser window
     driver.maximize_window()
     
-    # Create a Google Images search URL
     query_formatted = query.replace(" ", "%20")
     search_url = f"https://pixabay.com/images/search/{query_formatted}/"
 
-    # Open the Google Images search page
     driver.get(search_url)
     while imgs_saved < num_images:
         time.sleep(5)
         WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "link--WHWzm")))
 
-        # Get image elements
         img_elements = driver.find_elements(By.TAG_NAME, "img")
 
-        # Create the save directory on the desktop
         save_path = os.path.join(os.getcwd(), save_path)
         os.makedirs(save_path, exist_ok=True)
 
-        # Loop through the first num_images images
         for i, img_element in enumerate(img_elements):
             try:            
                 src = img_element.get_attribute('src')
                 if src != 'https://pixabay.com/static/img/blank.gif':
-                    # print(src)
                     print('\rProgress:\t{:.2f}%'.format((imgs_saved / num_images) *100), end="")
                     img_data = requests.get(src).content
                     with open(f"{save_path}/{query}_{imgs_saved}.jpg", 'wb') as handler:
@@ -64,26 +57,16 @@ def scrape_images(query, num_images, save_path):
         driver.get(search_url)
         page += 1
         
-    # Close the browser
     driver.quit()
 
-# query = "office laptop"
-# num_images = 250
-# save_path = "office laptop"
-# scrape_images(query, num_images, save_path)
-
-# query = "spoon"
-# num_images = 250
-# save_path = "spoon"
-# scrape_images(query, num_images, save_path)
-
-# query = "fork"
-# num_images = 250
-# save_path = "fork"
-# scrape_images(query, num_images, save_path)
-
-query = "eating utensils"
-num_images = 750
-save_path = "eating utensils"
+query = "laptop"
+num_images = 250
+save_path = "laptop"
 scrape_images(query, num_images, save_path)
+
+query = "book"
+num_images = 250
+save_path = "book"
+scrape_images(query, num_images, save_path)
+
 
